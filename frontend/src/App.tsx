758969b8fe8c8d2616433605329b290c414cd58f
Abhinav Robinson <abhinavrobinson@gmail.com>
2022-02-09 21:41:58 +0530

    feat(home): styles#5

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -111,6 +111,13 @@ function App() {
 						onClick={() => {
 							updateCurrentState(undefined)
 						}}
+						style={
+							{
+								position: 'absolute',
+								top: '10px',
+								right: '10px',
+							}
+						}
 					>
 						Back
 					</button>
