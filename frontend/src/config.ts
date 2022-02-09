1f7400d4b75f5713f9341bfc0a498052f5899bca
SurroundingArt <97761020+SurroundingArt64@users.noreply.github.com>
2022-02-09 23:28:00 +0530

    fix: transfer logic

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,29 @@
 export const DB_NAME = 'UsersDatabase'
 export const BLOCK_API_KEY = 'ed14d7c7-1532-4102-8078-6d2e2cd56aac'
 export const MORALIS_API_KEY = 'lmkNT0iJnRRuhioPyZtrb1Cz9C7WMJg7xnkKNMc9smRRVITmC7V7GRjssrHfoS6e'
+
+export const ERC721ABI = [
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'from',
+				type: 'address',
+			},
+			{
+				internalType: 'address',
+				name: 'to',
+				type: 'address',
+			},
+			{
+				internalType: 'uint256',
+				name: 'tokenId',
+				type: 'uint256',
+			},
+		],
+		name: 'transferFrom',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+]
