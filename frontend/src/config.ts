c4524c6897b727dfe37363025c24591ba7a21a95
SurroundingArt <97761020+SurroundingArt64@users.noreply.github.com>
2022-02-09 22:38:21 +0530

    feat: get image

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,3 @@
 export const DB_NAME = 'UsersDatabase'
 export const BLOCK_API_KEY = 'ed14d7c7-1532-4102-8078-6d2e2cd56aac'
+export const MORALIS_API_KEY = 'lmkNT0iJnRRuhioPyZtrb1Cz9C7WMJg7xnkKNMc9smRRVITmC7V7GRjssrHfoS6e'
