35dac4b5d3c723e90af4fc0276cb7c7296253b6a
AquaRules <aquarules22@gmail.com>
2022-02-09 20:40:59 +0530

    feat(login): add login

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,11 +1,16 @@
+import { Fluence } from '@fluencelabs/fluence'
+import { krasnodar } from '@fluencelabs/fluence-network-environment'
 import React from 'react'
+import { get_private_key_data } from '../_aqua/main'
 
 const Login: React.FC = () => {
   const [addr, setAddr] = React.useState("")
   const [pw, setPw] = React.useState("")
 
   const handleLogin = async (addr: string, pw: string) => {
-    console.log(addr, pw)
+    await Fluence.start({ connectTo: krasnodar[0] })
+    const res = await get_private_key_data("user20", addr, pw)
+    console.log(res)
   }
 
   return (
