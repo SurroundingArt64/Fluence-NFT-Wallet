2673c3463b33b3ab7adab1688dcf94607dcbae91
Abhinav Robinson <abhinavrobinson@gmail.com>
2022-02-10 20:19:10 +0530

    feat(styles): #23

--- frontend/src/components/NFTWallet.tsx
+++ frontend/src/components/NFTWallet.tsx
@@ -3,6 +3,19 @@ import { ethers } from 'ethers'
 import React, { useEffect, useState } from 'react'
 import { ERC721ABI, MORALIS_API_KEY } from '../config'
 
+export interface NFTItemProps {
+	token_address: string
+	token_id: string
+	owner_of: string
+	block_number: string
+	amount: string
+	contract_type: string
+	name: string
+	symbol: string
+	token_uri: string
+	metadata?: string
+}
+
 export const NFTWallet: React.FC<{
 	signer: ethers.Wallet
 	network: {
@@ -13,20 +26,8 @@ export const NFTWallet: React.FC<{
 		explorer: string
 	}
 }> = ({ signer, network }) => {
-	const [NFTs, setNFTs] = useState<
-		{
-			token_address: string
-			token_id: string
-			owner_of: string
-			block_number: string
-			amount: string
-			contract_type: string
-			name: string
-			symbol: string
-			token_uri: string
-			metadata: string | null
-		}[]
-	>([])
+	const [NFTs, setNFTs] = useState<NFTItemProps[]>([])
+	const [selectedNFT, setSelectedNFT] = useState<NFTItemProps>()
 
 	useEffect(() => {
 		if (network && signer) {
@@ -54,9 +55,12 @@ export const NFTWallet: React.FC<{
 	return (
 		<>
 			<h1>NFT Wallet</h1>
+			{selectedNFT && <SelectedNFTComponent {...{ signer, network }} elem={selectedNFT} />}
 			<div className='nft'>
 				{NFTs.length > 0 ? NFTs.map((elem, idx) => (
-					<NFTComponent signer={signer} network={network} idx={idx} elem={elem} />
+					<div key={idx} onClick={() => setSelectedNFT(elem)}>
+						<NFTComponent signer={signer} network={network} idx={idx} elem={elem} />
+					</div>
 				)) : (
 					<h2 style={{
 						height: 'calc(100vh - 60vh)',
@@ -83,18 +87,7 @@ function NFTComponent({
 		explorer: string
 	}
 	idx: number
-	elem: {
-		token_address: string
-		token_id: string
-		owner_of: string
-		block_number: string
-		amount: string
-		contract_type: string
-		name: string
-		symbol: string
-		token_uri: string
-		metadata: string | null
-	}
+	elem: NFTItemProps
 }): JSX.Element {
 	const [tokenURI, setTokenURI] = useState('')
 	const [transferToAddress, setTransferTo] = useState('')
@@ -242,6 +235,174 @@ function NFTComponent({
 						</div>
 					</form>)
 				}
+			</div>
+		</div>
+	)
+}
+
+function SelectedNFTComponent({
+	signer,
+	elem,
+	network: { explorer, chainId },
+}: {
+	signer: ethers.Wallet
+	network: {
+		name: string
+		chainId: number
+		rpcURL: string
+		moralisIdx: string
+		explorer: string
+	}
+	elem: NFTItemProps
+}): JSX.Element {
+	const [tokenURI, setTokenURI] = useState('')
+	const [transferToAddress, setTransferTo] = useState('')
+	const [tx, setTx] = useState()
+	useEffect(() => {
+		const run = async () => {
+			console.log(elem)
+			if (elem.token_uri) {
+				const resp = await axios.get<{ image: string; attributes: any[] }>(elem.token_uri)
+				if (resp.data.image.startsWith('ipfs://')) {
+					setTokenURI('https://ipfs.moralis.io:2053/ipfs/' + resp.data.image.split('ipfs://')[1])
+				} else {
+					setTokenURI(resp.data.image)
+				}
+			}
+		}
+		run()
+	}, [elem])
+	const getOpenSeaURL = () => {
+		let baseURL
+		switch (chainId) {
+			case 1:
+				baseURL = 'https://opensea.io/assets/'
+				break
+			case 4:
+				baseURL = 'https://testnets.opensea.io/assets/'
+				break
+			case 137:
+				baseURL = 'https://opensea.io/assets/polygon/'
+				break
+			case 80001:
+				baseURL = 'https://testnets.opensea.io/assets/mumbai/'
+				break
+			default:
+				break
+		}
+		return baseURL + elem.token_address + '/' + elem.token_id
+	}
+	const [transfer, setTransfer] = useState(false)
+
+	async function transferTo(address: string) {
+		const tx = await new ethers.Contract(elem.token_address, ERC721ABI)
+			.connect(signer)
+			.transferFrom(signer.address, address, elem.token_id)
+		console.log({ tx })
+		setTx(tx.hash)
+		const receipt = await tx.wait()
+		console.log({ receipt })
+	}
+
+	return (
+		<div className='nft-card-selected'>
+			<img src={tokenURI} alt="" />
+			<div className='container'>
+				<div className='headers'>
+					<div className='item'>
+						<h5>NFT Name</h5>
+						<p>{elem.name}</p>
+					</div>
+					<div className='item'>
+						<h5>TOKEN ID</h5>
+						<p>{elem.symbol}</p>
+					</div>
+					<div className='item'>
+						<h5>Visit Explorer</h5>
+						<p>
+							<a
+								href={explorer + 'token/' + elem.token_address + `?a=${elem.token_id}#inventory`}
+								target='_blank'
+								rel='noopener noreferrer'
+							>
+								↗
+							</a>
+						</p>
+					</div>
+					<div className='item'>
+						<h5>Visit Opensea</h5>
+						<p>
+							<a href={getOpenSeaURL()} target='_blank' rel='noopener noreferrer'>
+								↗
+							</a>
+						</p>
+					</div>
+					<div className="item">
+						<h5>Transfer to Address</h5>
+						<p onClick={() => {
+							setTransfer(!transfer)
+						}}
+							style={{ cursor: 'pointer' }}
+						>
+							↘
+						</p>
+					</div>
+					{tx && (
+						<>
+							<div className="item">
+								<h5>View Transaction</h5>
+								<p>
+									<a href={explorer + 'tx/' + tx} target='_blank' rel='noopener noreferrer'>
+										↗
+									</a>
+								</p>
+							</div>
+						</>
+					)}
+					{
+						transfer &&
+						(<form
+							onClick={(e) => {
+								e.preventDefault()
+								if (transferToAddress) transferTo(transferToAddress)
+							}}
+							style={{
+								border: '2px solid #ccc',
+								borderRadius: '10px',
+								padding: '10px',
+								display: 'flex',
+								alignItems: 'flex-start',
+							}}
+						>
+							<label style={{
+								fontSize: "1.2rem",
+							}}>Transfer ↗</label>
+							<div style={{
+								padding: 0,
+								width: "235px",
+								margin: "10px 0 0 0",
+							}}>
+								<input
+									type='text'
+									value={transferToAddress}
+									onChange={(e) => {
+										setTransferTo(e.target.value)
+									}}
+									id=''
+									placeholder='0x0000****0000'
+									style={{
+										minWidth: "100px",
+										width: "175px",
+										display: 'flex',
+										alignItems: 'center',
+										justifyContent: 'center',
+									}}
+								/>
+								<button type='submit'>Transfer NFT</button>
+							</div>
+						</form>)
+					}
+				</div>
 			</div>
 		</div>
 	)
