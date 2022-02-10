5de5a16f04df8850416875027e57de00500b6b0c
AquaRules <aquarules22@gmail.com>
2022-02-10 20:38:17 +0530

    feat(styles): #24

--- frontend/src/components/NFTWallet.tsx
+++ frontend/src/components/NFTWallet.tsx
@@ -58,18 +58,14 @@ export const NFTWallet: React.FC<{
 			<h1>NFT Wallet</h1>
 			{selectedNFT && <SelectedNFTComponent {...{ signer, network, seaport }} elem={selectedNFT} />}
 			<div className='nft'>
-				{NFTs.length > 0 ? (
-					NFTs.map((elem, idx) => (
-						<div key={idx} onClick={() => setSelectedNFT(elem)}>
-							<NFTComponent signer={signer} network={network} idx={idx} elem={elem} />
-						</div>
-					))
-				) : (
-					<h2
-						style={{
-							height: 'calc(100vh - 60vh)',
-						}}
-					>
+				{NFTs.length > 0 ? NFTs.map((elem, idx) => (
+					<div key={idx} onClick={() => setSelectedNFT(elem)}>
+						<NFTComponent {...{ network, elem }} />
+					</div>
+				)) : (
+					<h2 style={{
+						height: 'calc(100vh - 60vh)',
+					}}>
 						No NFTs found, choose another network?
 					</h2>
 				)}
@@ -78,12 +74,9 @@ export const NFTWallet: React.FC<{
 	)
 }
 function NFTComponent({
-	signer,
-	idx,
 	elem,
 	network: { explorer, chainId },
 }: {
-	signer: ethers.Wallet
 	network: {
 		name: string
 		chainId: number
@@ -91,12 +84,9 @@ function NFTComponent({
 		moralisIdx: string
 		explorer: string
 	}
-	idx: number
 	elem: NFTItemProps
 }): JSX.Element {
 	const [tokenURI, setTokenURI] = useState('')
-	const [transferToAddress, setTransferTo] = useState('')
-	const [tx, setTx] = useState()
 	useEffect(() => {
 		const run = async () => {
 			if (elem.token_uri) {
@@ -130,21 +120,9 @@ function NFTComponent({
 		}
 		return baseURL + elem.token_address + '/' + elem.token_id
 	}
-	const [transfer, setTransfer] = useState(false)
-
-	async function transferTo(address: string) {
-		const tx = await new ethers.Contract(elem.token_address, ERC721ABI)
-			.connect(signer)
-			.transferFrom(signer.address, address, elem.token_id)
-		console.log({ tx })
-		setTx(tx.hash)
-		const receipt = await tx.wait()
-		console.log({ receipt })
-	}
-
 	return (
-		<div key={idx} className='nft-card'>
-			<img src={tokenURI} alt='' />
+		<div className='nft-card'>
+			<img src={tokenURI} alt="" />
 			<div className='headers'>
 				<div className='item'>
 					<h5>NFT Name</h5>
@@ -174,77 +152,6 @@ function NFTComponent({
 						</a>
 					</p>
 				</div>
-				<div className='item'>
-					<h5>Transfer to Address</h5>
-					<p
-						onClick={() => {
-							setTransfer(!transfer)
-						}}
-						style={{ cursor: 'pointer' }}
-					>
-						↘
-					</p>
-				</div>
-				{tx && (
-					<>
-						<div className='item'>
-							<h5>View Transaction</h5>
-							<p>
-								<a href={explorer + 'tx/' + tx} target='_blank' rel='noopener noreferrer'>
-									↗
-								</a>
-							</p>
-						</div>
-					</>
-				)}
-				{transfer && (
-					<form
-						onClick={(e) => {
-							e.preventDefault()
-							if (transferToAddress) transferTo(transferToAddress)
-						}}
-						style={{
-							border: '2px solid #ccc',
-							borderRadius: '10px',
-							padding: '10px',
-							display: 'flex',
-							alignItems: 'flex-start',
-						}}
-					>
-						<label
-							style={{
-								fontSize: '1.2rem',
-							}}
-						>
-							Transfer ↗
-						</label>
-						<div
-							style={{
-								padding: 0,
-								width: '235px',
-								margin: '10px 0 0 0',
-							}}
-						>
-							<input
-								type='text'
-								value={transferToAddress}
-								onChange={(e) => {
-									setTransferTo(e.target.value)
-								}}
-								id=''
-								placeholder='0x0000****0000'
-								style={{
-									minWidth: '100px',
-									width: '175px',
-									display: 'flex',
-									alignItems: 'center',
-									justifyContent: 'center',
-								}}
-							/>
-							<button type='submit'>Transfer NFT</button>
-						</div>
-					</form>
-				)}
 			</div>
 		</div>
 	)
@@ -327,111 +234,94 @@ function SelectedNFTComponent({
 	}
 
 	return (
-		<div className='nft-card-selected'>
-			<img src={tokenURI} alt='' />
-			<div className='container'>
-				<div className='headers'>
-					<div className='item'>
-						<h5>NFT Name</h5>
-						<p>{elem.name}</p>
+		<div className='nft-select'>
+			<div className="nft-card-selected">
+				<img src={tokenURI} alt="" />
+				<div className='container'>
+					<div className='headers'>
+						<div className='item'>
+							<h5>NFT Name</h5>
+							<p>{elem.name}</p>
+						</div>
+						<div className='item'>
+							<h5>TOKEN ID</h5>
+							<p>{elem.symbol}</p>
+						</div>
+						<div className='item'>
+							<h5>Visit Explorer</h5>
+							<p>
+								<a
+									href={explorer + 'token/' + elem.token_address + `?a=${elem.token_id}#inventory`}
+									target='_blank'
+									rel='noopener noreferrer'
+								>
+									↗
+								</a>
+							</p>
+						</div>
+						<div className='item'>
+							<h5>Visit Opensea</h5>
+							<p>
+								<a href={getOpenSeaURL()} target='_blank' rel='noopener noreferrer'>
+									↗
+								</a>
+							</p>
+						</div>
+						{tx && (
+							<>
+								<div className="item">
+									<h5>View Transaction</h5>
+									<p>
+										<a href={explorer + 'tx/' + tx} target='_blank' rel='noopener noreferrer'>
+											↗
+										</a>
+									</p>
+								</div>
+							</>
+						)}
 					</div>
-					<div className='item'>
-						<h5>TOKEN ID</h5>
-						<p>{elem.symbol}</p>
-					</div>
-					<div className='item'>
-						<h5>Visit Explorer</h5>
-						<p>
-							<a
-								href={explorer + 'token/' + elem.token_address + `?a=${elem.token_id}#inventory`}
-								target='_blank'
-								rel='noopener noreferrer'
-							>
-								↗
-							</a>
-						</p>
-					</div>
-					<div className='item'>
-						<h5>Visit Opensea</h5>
-						<p>
-							<a href={getOpenSeaURL()} target='_blank' rel='noopener noreferrer'>
-								↗
-							</a>
-						</p>
-					</div>
-					<div className='item'>
-						<h5>Transfer to Address</h5>
-						<p
-							onClick={() => {
-								setTransfer(!transfer)
-							}}
-							style={{ cursor: 'pointer' }}
-						>
-							↘
-						</p>
-					</div>
-					{tx && (
-						<>
-							<div className='item'>
-								<h5>View Transaction</h5>
-								<p>
-									<a href={explorer + 'tx/' + tx} target='_blank' rel='noopener noreferrer'>
-										↗
-									</a>
-								</p>
-							</div>
-						</>
-					)}
-					{transfer && (
-						<form
-							onClick={(e) => {
-								e.preventDefault()
-								if (transferToAddress) transferTo(transferToAddress)
-							}}
-							style={{
-								border: '2px solid #ccc',
-								borderRadius: '10px',
-								padding: '10px',
-								display: 'flex',
-								alignItems: 'flex-start',
-							}}
-						>
-							<label
-								style={{
-									fontSize: '1.2rem',
-								}}
-							>
-								Transfer ↗
-							</label>
-							<div
-								style={{
-									padding: 0,
-									width: '235px',
-									margin: '10px 0 0 0',
-								}}
-							>
-								<input
-									type='text'
-									value={transferToAddress}
-									onChange={(e) => {
-										setTransferTo(e.target.value)
-									}}
-									id=''
-									placeholder='0x0000****0000'
-									style={{
-										minWidth: '100px',
-										width: '175px',
-										display: 'flex',
-										alignItems: 'center',
-										justifyContent: 'center',
-									}}
-								/>
-								<button type='submit'>Transfer NFT</button>
-							</div>
-						</form>
-					)}
 				</div>
 			</div>
+			<form
+				onClick={(e) => {
+					e.preventDefault()
+					if (transferToAddress) transferTo(transferToAddress)
+				}}
+				style={{
+					border: '2px solid #ccc',
+					borderRadius: '10px',
+					padding: '10px',
+					display: 'flex',
+					alignItems: 'flex-start',
+				}}
+			>
+				<label style={{
+					fontSize: "1.2rem",
+				}}>Transfer ↗</label>
+				<div style={{
+					padding: 0,
+					width: "235px",
+					margin: "10px 0 0 0",
+				}}>
+					<input
+						type='text'
+						value={transferToAddress}
+						onChange={(e) => {
+							setTransferTo(e.target.value)
+						}}
+						id=''
+						placeholder='0x0000****0000'
+						style={{
+							minWidth: "100px",
+							width: "175px",
+							display: 'flex',
+							alignItems: 'center',
+							justifyContent: 'center',
+						}}
+					/>
+					<button type='submit'>Transfer NFT</button>
+				</div>
+			</form>
 		</div>
 	)
 }
